86eaf32bd18a29ba8cd5039f2f214b52c5ca3371
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 18:01:27 +0200

    third migration

--- photos/models.py
+++ photos/models.py
@@ -43,3 +43,25 @@ class Location(models.Model):
 
 class Category(models.Model):
     photo_category=models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+    def delete_category(self):
+        self.delete()
+    
+    def update_category(self,update):
+        self.photo_category=update
+        self.save()
+    @classmethod
+    def get_category_id(cls,id):
+        category=Category.objects.get(pk=id)
+        return category
+
+    @classmethod
+    def search_by_category(cls,id):
+        category=Category.objects.get(pk=id)
+        return category
+    
+    def __str__(self):
+        return self.photo_category
+
