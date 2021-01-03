c6cde6a1fe31d2a8a25af8d76a79c108c05226af
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 15:28:33 +0200

    setting routes to different views

--- photos/urls.py
+++ photos/urls.py
@@ -2,3 +2,10 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^search/',  views.search_results,name='search_results'),
+    url(r'^location/(\d+)',  views.filter_by_location,name='location'),
+    url(r'^image/(\d+)',  views.image,name='image')
+]
