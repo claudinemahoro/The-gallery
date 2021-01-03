98957e4063a0ec02325bee26df5ddd2a6f6e8ef7
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 15:20:58 +0200

    defining the location view

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def location (request,id):
+    location=Image.filter_by_location(id=id)
+    print(location)
+    locate=Location.objects.all()
+    return render(request,'location.html',{"location":location,'locate':locate})
