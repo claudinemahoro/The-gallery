f1662c53f402e112eba652cfe0f4999bfbdda83f
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 16:52:11 +0200

    creating photographer class

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+
+class Photographer(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    def __str__(self):
+        return self.first_name
+    
