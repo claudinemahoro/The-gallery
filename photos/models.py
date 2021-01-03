be75b917e2ee03390b3fd081424f5471f83da064
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 17:23:16 +0200

    editing photographer model

---
+++ photos/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+import datetime as dt
+
+class Photographer(models.Model):
+    first_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    def __str__(self):
+        return self.first_name
+    def save_photographer(self):
+        self.save()
+    def delete_photographer(self):
+        self.delete()
+    @classmethod
+    def display_photographers(cls):
+        photographers=Photographer.objects.all()
+        for photographer in photographers:
+            return photographer
+    class Meta:
+        ordering=['first_name']
