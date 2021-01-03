6a427f306db87bb77bccb6a1b93caab0bf77f651
claudinemahoro <claudinemah3@gmail.com>
2021-01-03 18:02:55 +0200

    the image model

--- photos/models.py
+++ photos/models.py
@@ -65,3 +65,45 @@ class Category(models.Model):
     def __str__(self):
         return self.photo_category
 
+class Image(models.Model):
+    title=models.CharField(max_length=30)
+    description=models.TextField(max_length=30)
+    image=models.ImageField(upload_to='images_galleries/')
+    location=models.ForeignKey(Location, on_delete=models.DO_NOTHING)
+    category=models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    photographer=models.ForeignKey(Photographer, on_delete=models.DO_NOTHING)
+    pub_date=models.DateTimeField(auto_now_add=True,null=True)
+
+    class Meta:
+        ordering=('-id',)
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+    
+    def update_category(self,update):
+        self.photo_category=update
+        self.save()
+        
+    @classmethod
+    def get_all_image(cls):
+        all_images=Image.objects.all()
+        return all_images
+    
+    @classmethod
+    def get_image_by_id(cls,id):
+        an_image=Image.objects.get(id=id)
+        return an_image
+
+    def __str__(self):
+        return self.title
+    @classmethod
+    def search_by_category(cls,search_term):
+        photo=cls.objects.filter(category__photo_category__icontains=search_term)
+        return photo
+    
+    @classmethod
+    def filter_by_location(cls,id):
+        images=Image.objects.filter(location_id=id)
+        return images
+   
